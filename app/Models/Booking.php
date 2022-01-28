380861d1b2a2b3034b956b90bf6ef53440645002
Sean Smith <smith.sean1998@gmail.com>
2022-01-28 19:17:38 +0000

    added booking model and controller

---
+++ app/Models/Booking.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'booking_date',
+        'flexibility',
+        'vehicle_size',
+        'contact_number',
+        'email_address'
+    ];
+}
---
+++ database/migrations/2022_01_28_190719_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->datetime('booking_date');
+            $table->integer('flexibility');
+            $table->string('vehicle_size');
+            $table->bigInteger('contact_number');
+            $table->string('email_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
